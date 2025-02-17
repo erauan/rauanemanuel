728f4320b66c51f72abaf22a170353de620a7010
erauan <rauanemanuel@gmail.com>
2025-02-16 23:45:01 -0300

    segurança basica da pragina

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+if (document.addEventListener) {
+    document.addEventListener("keydown", bloquearSource);
+} else { //Versões antigas do IE
+    document.attachEvent("onkeydown", bloquearSource);
+}
+
+function bloquearSource(e) {
+    e = e || window.event;
+
+    var code = e.which || e.keyCode;
+
+    if (
+        e.ctrlKey &&
+        (code == 83 || code == 85) //83 = S, 85 = U
+    ) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
+
+        return false;
+    }
+} 
+
 document.getElementById('btnProjects').addEventListener('click', function() {
     document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
 });
